db2579b168ff397a15fcd9c21d31234d7f5efa5b
Bharat-34 <bharatkumargude@gmail.com>
2019-12-15 16:09:36 +0530

    newqqq

--- Quin Demo/ViewController.swift
+++ Quin Demo/ViewController.swift
@@ -85,7 +85,7 @@ class ViewController: UIViewController {
             
             catch {
                 print("error")
-                print("hhhhhh")
+                print("hhhhhhhhhh")
             }
 
             }
